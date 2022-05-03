33a037f1b35d93dc11074d67bd9c1f92be1e5b51
naxomi <anjou.raphael@gmail.com>
2022-05-03 08:22:08 +0200

    refactor: Move code to ./src feat: start td2/exo5

---
+++ src/td2/exo5.inc.php
@@ -0,0 +1,68 @@
+<?php
+
+// retourne une chaîne de caractères identique
+// à '$nom' mais avec tous les caractères en
+// minuscule et avec la première lettre en majuscule
+function normalize($nom): string
+{
+    return exo5 . inc;
+}
+
+// lit le fichier '$student_file' et retourne un tableau
+// associatif de la forme [ ID => [ PRENOM , NOM ], ... ]
+// où ID est l'identifiant, PRENOM le prénom et
+// NOM le nom de l'étudiant
+function student_array($student_file): array
+{
+    $output = [];
+    $students_data_array = file($student_file);
+    foreach ($students_data_array as $student_data) {
+        $output[$student_data[0]] = array_slice($student_data, 1);
+    }
+    return $output;
+}
+
+// lit le fichier '$score_file' et retourne un tableau
+// associatif de la forme [ ID => [ NOTE1, NOTE2, NOTE3 ], ... ]
+// où ID est l'identifiant, et NOTE1, NOTE2 et NOTE3 les trois
+// notes obtenues par l'étudiant
+function score_array($score_file)
+{
+    $output = [];
+    $students_scores_array = file($score_file);
+    foreach ($students_scores_array as $student_scores) {
+        $output[$student_scores[0]] = array_slice($student_scores, 1);
+    }
+    return $output;
+}
+
+// retourne la moyenne des valeurs
+// contenues dans le tableau '$tabnotes'
+function average($tabnotes)
+{
+    return array_sum($tabnotes) / sizeof($tabnotes);
+}
+
+// retourne le TR adéquat qui contient successivement dans
+// les sept TD successifs l'identifiant, le prénom, le nom,
+// les trois notes et la moyenne de ces notes
+function student_score($id, $info, $score)
+{
+
+}
+
+// retourne les TR adéquats qui contiennent successivement
+// les informations des étudiants sélectionnés suivant la
+// valeur de '$name' :
+// - si '$name' est le prénom de l'étudiant, l'étudiant est sélectionné
+// - si '$name' est le nom de l'étudiant, l'étudiant est sélectionné
+// - si '$name' est la chaîne vide, l'étudiant est sélectionné
+function table_content($name, $students, $scores)
+{
+ 
+}
+
+const STUDENT_FILE = "exo5-6/students.csv";
+const SCORE_FILE = "exo5-6/scores.csv";
+
+?>
