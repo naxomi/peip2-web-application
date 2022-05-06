2c02d341b4d839a8a7b57fd46e31145e07c31644
naxomi <anjou.raphael@gmail.com>
2022-05-06 14:45:01 +0200

    feat: add td3 - exo2

--- src/td3/exo1/formulaire.php
+++ src/td3/exo1/formulaire.php
@@ -1,30 +1,35 @@
 <?php
-	session_start();
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-	<head>
-		<meta charset="utf-8">
-		<title>TP 3 - Exo 1</title>
-		<meta name="author" content="Marc Gaetano">
-		<meta name="viewport" content="width=device-width; initial-scale=1.0">
-		<link rel="stylesheet" href="../css/tp3.css">
-	</head>
+<head>
+    <meta charset="utf-8">
+    <title>TP 3 - Exo 1</title>
+    <meta name="author" content="Marc Gaetano">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0">
+    <link rel="stylesheet" href="../css/tp3.css">
+</head>
 <?php
-    // à compléter
-    
+// à compléter
+
 ?>
-	<body>
-		<h1>TP 3 - Exo 1</h1>
-		<hr>
+<body>
+<h1>TP 3 - Exo 1</h1>
+<hr>
 
-		<h2>Multiplication</h2>
-			
-		<form action="action.php" method="get">
-			<?php echo $x; ?> x <?php echo $y; ?> = <input type="text" name="utilisateur">
-			<br><br>
-			<input type="submit" value="Envoyer">
-		</form>
+<h2>Multiplication</h2>
 
-	</body>
+<form action="action.php" method="get">
+    <?php
+        $x = $_SESSION['x'] = rand(2, 10);
+        $y = $_SESSION['y'] = rand(2, 10);
+        $label = "<label>$x * $y = </label>";
+    ?>
+    <?php echo $x; ?> x <?php echo $y; ?> = <input type="text" name="utilisateur">
+    <br><br>
+    <input type="submit" value="Envoyer">
+</form>
+
+</body>
 </html>
