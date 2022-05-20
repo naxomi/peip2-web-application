7484c2a4493af9298e40b9bc141541536a5eaec0
neoteristis <anjou.raphael@gmail.com>
2022-05-20 15:09:10 +0200

    feat: td4.exo3

--- src/td4/exo3.js
+++ src/td4/exo3.js
@@ -1,18 +1,20 @@
-
-var x; // le premier nombre de la multiplication
-var y; // le deuxième nombre de la multiplication
+let x; // le premier nombre de la multiplication
+let y; // le deuxième nombre de la multiplication
 
 // donne le rôle du bouton :
 // si 'verifier' est 'true' alors le prochain clic sur le bouton
 // est destiné à vérifier la réponse de l'utilisateur, sinon,
 // le clic est destiné à proposer une nouvelle multiplication
-var verifier = true; 
+let verifier = true;
 
-// génére une nouvelle multiplication, autrement dit :
+// génère une nouvelle multiplication, autrement dit :
 // - génère deux entiers au hasard dans l'intervalle [1,9]
 // - les affiche dans les bons éléments HTML
 function nouvelle() {
-
+    x = Math.floor((Math.random() * 9) + 1);
+    y = Math.floor((Math.random() * 9) + 1);
+    document.getElementById("nombre1").innerHTML = x.toString();
+    document.getElementById("nombre2").innerHTML = y.toString();
 }
 
 // cette fonction est appelée quand l'utilisateur clique
@@ -21,7 +23,26 @@ function nouvelle() {
 // - générer une nouvelle multiplication
 // Cette alternance est gérée à l'aide de la variable 'verifier'
 function valider() {
-
+    if (verifier) {
+        let answer = parseFloat(document.getElementById("proposition").value);
+        if (isNaN(answer)) {
+            alert("You must input a number");
+        } else {
+            if (x * y === answer) {
+                document.getElementById("resultat").innerHTML = "Bonne réponse";
+            } else {
+                document.getElementById("resultat").innerHTML = "Mauvaise réponse";
+            }
+            document.getElementById("bouton").value = "continuer";
+            document.getElementById("resultat").style.visibility = "visible";
+        }
+    } else {
+        document.getElementById("resultat").style.visibility = "hidden";
+        document.getElementById("bouton").value = "verifier";
+        document.getElementById("proposition").value = "";
+        nouvelle();
+    }
+    verifier = !verifier;
 }
 
 
