10ed9e67a3d97c75488f26e047b19054efaa0602
neoteristis <anjou.raphael@gmail.com>
2022-05-20 14:34:47 +0200

    feat: td4.exo1

--- src/td4/exo1.js
+++ src/td4/exo1.js
@@ -1,8 +1,16 @@
-
 // calcule le prix TTC à partir du prix hors-taxe
 // et de la TVA
 // affiche une alerte avec un message d'erreur si les
 // valeurs fournies ne sont pas des nombres
 function prixTTC() {
+    let priceHT = parseFloat(document.getElementById("pht").value);
+    let TVA = parseFloat(document.getElementById("tva").value) / 100;
 
+    if (isNaN(priceHT) || isNaN(TVA)) {
+        alert('You can only input numbers.');
+    } else {
+        let priceTTC = priceHT * (TVA + 1);
+        document.getElementById("pttc").innerHTML = priceTTC.toFixed(2);
+        document.getElementById("resultat").style.visibility = "visible";
+    }
 }
