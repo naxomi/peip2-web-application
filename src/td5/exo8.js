7a1afe5e1b6ddd405e8094c88318435d73c62e8e
neoteristis <anjou.raphael@gmail.com>
2022-06-04 14:45:56 +0200

    feat: add resources for td5

---
+++ src/td5/exo8.js
@@ -0,0 +1,32 @@
+
+window.onload = function () {
+
+	// le "handler" du setTimeout
+	let chrono = null;
+
+	// si 'ok' est 'true', alors l'utilisateur
+	// a choisi la bonne réponse
+	let ok = false;
+
+	// affiche le message 'm' avec la couleur 'c'
+	// dans l'élément prévu à cet effet
+	function msg(m, c) {
+		
+
+	}
+
+	// cette fonction est appelée à l'issue
+	// du setTimeout
+	function stop() {
+		
+	}
+
+	// traite le "clic" sur un bouton radio
+	function verifier() {
+		
+	}
+
+	// ici, on lance le setTimeout et stocke
+	// le "handler" dans la variable 'chrono'
+	
+};
